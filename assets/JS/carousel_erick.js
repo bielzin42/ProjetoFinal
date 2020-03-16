b67f35e952043fe8bc62bfeab7a0a9c93336629e
Gabriel Lima <nickspoot@gmail.com>
2020-03-16 15:27:20 -0300

    revendo carousel

--- assets/JS/carousel_erick.js
+++ assets/JS/carousel_erick.js
@@ -2,21 +2,21 @@ window.load = slide(1);
 
 var bgNumber = 1;
 
-function slide(n){
-    var allBgs = 5;
-    
-    document.getElementById('imagemBg').style.backgroundImage = "url('assets/img/"+n+".jpg ')";
-    
+function slide(n) {
+  var allBgs = 5;
+
+  document.getElementById("imagemBg").style.backgroundImage =
+    "url('assets/img/" + n + ".jpg ')";
 }
-function anterior(){
-    if(bgNumber>1){
-        bgNumber--;
-        slide(bgNumber);
-    }
+function anterior() {
+  if (bgNumber > 1) {
+    bgNumber--;
+    slide(bgNumber);
+  }
 }
-function proximo(){
-    if(bgNumber<5){
-        bgNumber++;
-        slide(bgNumber);
-    }
+function proximo() {
+  if (bgNumber < 5) {
+    bgNumber++;
+    slide(bgNumber);
+  }
 }
